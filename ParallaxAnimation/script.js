be1e709a3dbbfde720b595371af6c44577b59825
Shubham Ranjan <shubhamranjan542@gmail.com>
2020-06-13 16:51:00 +0530

    Parallax Animation

---
+++ ParallaxAnimation/script.js
@@ -0,0 +1,19 @@
+
+
+let bg = document.getElementById("bg");
+let moon = document.getElementById("moon");
+let mountain = document.getElementById("mountain");
+let road = document.getElementById("road");
+let text = document.getElementById("text");
+
+
+window.addEventListener('scroll', function(){
+    var value = this.window.scrollY;
+
+    bg.style.top = value * 0.5 + 'px';
+    moon.style.left = -value * 0.5 + 'px';
+    moon.style.top = value * 0.15 + 'px';
+    mountain.style.top = -value * 0.15 + 'px';
+    road.style.top = value * 0.15 + 'px';
+    text.style.top = value * 1 + 'px';
+});
